05f149eccd242f429208586af65e873d9eee25f0
stels24 <166283589+stels24@users.noreply.github.com>
2024-08-14 16:30:11 +0300

    Add files via upload

---
+++ Инлайн клавиатуры.py
@@ -0,0 +1,85 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button = KeyboardButton(text='Рассчитать')
+button1 = KeyboardButton(text='Информация')
+kb.add(button)
+kb.add(button1)
+
+inline_kb = InlineKeyboardMarkup(resize_keyboard=True)
+inline_button = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+inline_button1 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+inline_kb.add(inline_button)
+inline_kb.add(inline_button1)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message):
+    await message.answer('Выберите опцию:', reply_markup=inline_kb)
+
+
+@dp.message_handler(text='Информация')
+async def info(message):
+    await message.answer('Формула Миффлина-Сан Жеора – это одна из самых последних формул расчета калорий для '
+                         'оптимального похудения или сохранения нормального веса. Она была выведена в 2005 году '
+                         'и стала заменять классическую формулу Харриса-Бенедикта.')
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5')
+    await call.answer()
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    result_prog = int(10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) + 5)
+    await message.answer(f'Ваша норма калорий {result_prog} день')
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
